cdb8349356f3e2972ad2cbce93660c2270931634
salaheddinos <82298527+salaheddinos@users.noreply.github.com>
2021-06-21 18:39:51 +0100

    Update and rename NashVille_Dataset_Clean to NashVille_Dataset_Clean.sql

--- NashVille_Dataset_Clean.sql
+++ NashVille_Dataset_Clean.sql
@@ -77,23 +77,27 @@ WHERE
 ALTER TABLE nashville_housing.nashville
 	ADD Property_Address TEXT
 		AFTER PropertyAddress,
-    ADD Owner_Address TEXT
+    	ADD Owner_Address TEXT
 		AFTER OwnerAddress;
         
 UPDATE nashville_housing.nashville
 	SET 
 		Property_Address = coalesce(PropertyAddress, OwnerAddress, NULL),
-        Owner_Address = coalesce(OwnerAddress,PropertyAddress , NULL);
+        	Owner_Address = coalesce(OwnerAddress,PropertyAddress , NULL);
 
 ALTER TABLE nashville_housing.nashville
 	DROP COLUMN PropertyAddress, 
-    DROP COLUMN OwnerAddress;
+    	DROP COLUMN OwnerAddress;
 
 # We still have some missing values so we'll replace them by using addresses with same parcelID
 # To do that we need to first create a temp table then Update the records in our original table
 CREATE TEMPORARY TABLE B
-SELECT a.ParcelID AS ID, coalesce(a.Property_Address, b.Property_Address, NULL) AS ADDRESS
-FROM nashville_housing.nashville a 
+SELECT 
+	a.ParcelID AS ID, 
+	coalesce(a.Property_Address,
+	b.Property_Address, NULL) AS ADDRESS
+FROM 
+	nashville_housing.nashville a 
 JOIN nashville_housing.nashville b
 ON a.ParcelID = b.ParcelID
 AND a.UniqueID <> b.UniqueID
@@ -104,37 +108,47 @@ SET nashville_housing.nashville.Property_Address = B.ADDRESS
 WHERE nashville_housing.nashville.ParcelID = B.ID;
 
 -- Check modifications were succesfull for one record
-SELECT ParcelID, Property_Address
-FROM nashville_housing.nashville 
-WHERE UniqueID IN (SELECT ID FROM B) ;
+SELECT 
+	ParcelID, 
+	Property_Address
+FROM 
+	nashville_housing.nashville 
+WHERE 
+	UniqueID IN (SELECT ID FROM B) ;
 
 -- Breaking Address into individual cols (Address, City, State) 
 SELECT 
-	Owner_Address,
+    Owner_Address,
     SUBSTRING_INDEX(Owner_Address, ",", 1) AS ADDRESS,
     SUBSTRING_INDEX(SUBSTRING_INDEX(Owner_Address, ",", 2), ",", -1 ) AS City,
     SUBSTRING_INDEX(SUBSTRING_INDEX(Owner_Address, ",", -2), ",", -1 ) AS State
-FROM nashville_housing.nashville;
+FROM 
+	nashville_housing.nashville;
 
 # Add these values into new cols
 ALTER TABLE nashville_housing.nashville
 	ADD Property_Address_Split TEXT,
-    ADD Property_City_Split TEXT,
-    ADD Property_State_Split TEXT
+    	ADD Property_City_Split TEXT,
+    	ADD Property_State_Split TEXT
 		AFTER Property_Address;    
 
 UPDATE nashville_housing.nashville
 	SET 
-		Property_Address_Split = SUBSTRING_INDEX(Owner_Address, ",", 1),
+	Property_Address_Split = SUBSTRING_INDEX(Owner_Address, ",", 1),
         Property_City_Split = SUBSTRING_INDEX(SUBSTRING_INDEX(Owner_Address, ",", 2), ",", -1 ),
         Property_State_Split = SUBSTRING_INDEX(SUBSTRING_INDEX(Owner_Address, ",", -2), ",", -1 );
 
 # Inspect data
-SELECT * FROM nashville_housing.nashville;
+SELECT 
+	*
+FROM
+	nashville_housing.nashville;
 
 # Change SoldAsVacant values to Y and N
-SELECT DISTINCT SoldAsVacant
-FROM nashville_housing.nashville;   
+SELECT 
+	DISTINCT SoldAsVacant
+FROM 
+	nashville_housing.nashville;   
 
 UPDATE nashville_housing.nashville
 SET 
@@ -147,8 +161,9 @@ SET
 # Verify query
 SELECT 
 	DISTINCT SoldAsVacant,
-    COUNT(SoldAsVacant) AS num
-FROM nashville_housing.nashville
+    	COUNT(SoldAsVacant) AS num
+FROM 
+	nashville_housing.nashville
 GROUP BY SoldAsVacant
 ORDER BY num DESC;
 
@@ -157,9 +172,9 @@ ORDER BY num DESC;
 SELECT
 	UniqueID,
 	ParcelID, COUNT(ParcelID),
-    Property_Address, COUNT(Property_Address),
-    SalePrice, COUNT(SalePrice), 
-    SaleDate, COUNT(SaleDate)
+    	Property_Address, COUNT(Property_Address),
+    	SalePrice, COUNT(SalePrice), 
+    	SaleDate, COUNT(SaleDate)
 FROM 
 	nashville_housing.nashville
 GROUP BY 
@@ -175,8 +190,12 @@ HAVING
     ;
     
 # Verify Query for this parcelID, we can indeed see that this row is a duplicate     
-SELECT * FROM nashville_housing.nashville 
-WHERE ParcelID = '034 05 0 041.00';
+SELECT 
+	*
+FROM
+	nashville_housing.nashville 
+WHERE 
+	ParcelID = '034 05 0 041.00';
 
 # Now we're going to use the UniqueIDs found in the query above for removing duplicates
 # To make things easier, we're going to put the query into a temp table then filter out the UniqueIDs that contain duplicates
@@ -185,23 +204,26 @@ CREATE TEMPORARY TABLE duplicates
 SELECT
 	UniqueID,
 	ParcelID, COUNT(ParcelID),
-    Property_Address, COUNT(Property_Address),
-    SalePrice, COUNT(SalePrice), 
-    SaleDate, COUNT(SaleDate)
+    	Property_Address, COUNT(Property_Address),
+    	SalePrice, COUNT(SalePrice), 
+    	SaleDate, COUNT(SaleDate)
 FROM 
 	nashville_housing.nashville
 GROUP BY 
 	ParcelID,
-    Property_Address,
-    SalePrice,
-    SaleDate
+    	Property_Address,
+    	SalePrice,
+    	SaleDate
 HAVING 
 	COUNT(ParcelID) > 1
 	AND COUNT(Property_Address) > 1
-    AND COUNT(SalePrice) > 1
-    AND COUNT(SaleDate) > 1
+    	AND COUNT(SalePrice) > 1
+    	AND COUNT(SaleDate) > 1
     ;
 
 # Delete the duplicates
-DELETE FROM nashville_housing.nashville 
-WHERE UniqueID IN (SELECT UniqueID FROM duplicates);
+DELETE 
+	FROM 
+		nashville_housing.nashville 
+			WHERE 
+				UniqueID IN (SELECT UniqueID FROM duplicates);
